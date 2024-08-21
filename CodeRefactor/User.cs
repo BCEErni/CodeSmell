b8f38908cf915c6f5144465f9d9283aa6f4111d4
Benedict <benedict.escobar@betterask.erni>
2024-08-21 10:43:32 +0800

    first commit

---
+++ CodeRefactor/User.cs
@@ -0,0 +1,17 @@
+using System.Dynamic;
+
+class Users{
+    public int Id {get; set;}
+    public string? Name {get; set;}
+    public string? Company{get; set;}
+    public string? Username {get; set;}
+    public string? Email {get; set;}
+    public string? Address {get; set;}
+    public string? Zip{get; set;}
+    public string? State{get; set;}
+    public string? Country{get; set;}
+    public string? Phone{get; set;}
+    public string? Photo{get; set;}
+
+
+}
