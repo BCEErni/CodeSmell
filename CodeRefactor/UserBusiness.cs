838c26acc470d24277326bde070a2f527b0f3932
HMErni <hero.misa@betterask.erni>
2024-09-03 18:14:54 +0800

    code review

--- CodeRefactor/UserBusiness.cs
+++ CodeRefactor/UserBusiness.cs
@@ -4,10 +4,12 @@ public class UserBusiness
 {
     private readonly UserData _userdata;
 
-    public UserBusiness(){
+    public UserBusiness()
+    {
+        // Please make use of DI to inject the dependency.
         _userdata = new UserData();
     }
-     public async Task<List<Users>?> FetchUsersAsync(string apiUrl)
+    public async Task<List<Users>?> FetchUsersAsync(string apiUrl)
     {
         return await _userdata.GetUsersAsync(apiUrl);
     }
